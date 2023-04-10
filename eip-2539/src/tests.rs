66d0fa3cbce0421b35daa363b42b806d3681570e
echo <hujw77@gmail.com>
2023-04-10 16:33:27 +0800

    gen g1 add fail test

--- eip-2539/src/tests.rs
+++ eip-2539/src/tests.rs
@@ -1,7 +1,7 @@
 use ark_bls12_377::{Fq, Fr, G1Affine, G1Projective as G1, G2Affine, G2Projective as G2};
-use ark_ec::CurveGroup;
+use ark_ec::{CurveGroup, Group};
 use ark_ff::PrimeField;
-use ark_std::ops::Mul;
+use ark_std::ops::{Mul, Neg};
 use ark_std::test_rng;
 use ark_std::UniformRand;
 use ark_std::Zero;
@@ -41,6 +41,68 @@ fn write_vectors_fail(vectors: Vec<VectorFail>, name: &str) {
     let mut file = File::create(PREFIX.to_string() + name + ".json").expect("must create the file");
     file.write(serialized.as_bytes())
         .expect("must write vectors");
+}
+
+fn gen_fail_vectors(input_len: usize) -> Vec<VectorFail> {
+    let mut vectors: Vec<VectorFail> = vec![];
+
+    // invalid length: empty
+    {
+        let vector = VectorFail {
+            input: String::from(""),
+            expected_error: String::from("invalid input length"),
+            name: format!("invalid_input_length_empty"),
+        };
+        vectors.push(vector);
+    }
+
+    // invalid length: short
+    {
+        let vector = VectorFail {
+            input: String::from(""),
+            expected_error: String::from("invalid input length"),
+            name: format!("invalid_input_length_short"),
+        };
+        vectors.push(vector);
+    }
+
+    // invalid length: long
+    {
+        let input: String = hex::encode(vec![1u8; input_len + 1]);
+        let vector = VectorFail {
+            input,
+            expected_error: String::from("invalid input length"),
+            name: format!("invalid_input_length_large"),
+        };
+        vectors.push(vector);
+    }
+
+    // violate top zeros
+    {
+        let input: String = hex::encode(vec![1u8; input_len]);
+        let vector = VectorFail {
+            input,
+            expected_error: String::from("invliad Fq"),
+            name: format!("violate_top_zero_bytes"),
+        };
+        vectors.push(vector);
+    }
+
+    vectors
+}
+
+fn number_larger_than_modulus() -> Vec<u8> {
+    hex::decode("01ae3a4617c510eac63b05c06ca1493b1a22d9f300f5138f1ef3622fba094800170b5d44300000008508c00000000002")
+        .expect("must decode")
+}
+
+fn rand_g1_point_not_on_curve() -> G1 {
+    let mut rng = test_rng();
+    let x = Fq::rand(&mut rng);
+    let y = Fq::rand(&mut rng);
+    let p = G1Affine::new_unchecked(x, y);
+    assert!(!p.is_on_curve());
+    p.into()
 }
 
 fn encode_fq(field: Fq) -> [u8; 64] {
@@ -264,24 +326,196 @@ fn gen_g2_multiexp_vectors() {
     write_vectors(vectors, "G2MultiExp");
 }
 
-#[test]
-fn generate_test_vectors() {
-    // gen_g1_add_vectors();
-    // gen_g1_mul_vectors();
-    // gen_g1_multiexp_vectors();
-    // gen_g2_add_vectors();
-    // gen_g2_mul_vectors();
-    gen_g2_multiexp_vectors();
-    // gen_pairing_vectors();
+fn gen_pairing_vectors() {
+    let mut rng = test_rng();
+    let mut vectors: Vec<VectorSuccess> = vec![];
+    let mut positive_result_bytes: Vec<u8> = vec![0u8; 32];
+    positive_result_bytes[31] = 1u8;
+    let negative_result_bytes: Vec<u8> = vec![0u8; 32];
+    let g1_inf_encoded: Vec<u8> = vec![0u8; 128];
+    let g2_inf_encoded: Vec<u8> = vec![0u8; 256];
+
+    let g1 = G1::generator();
+    let g2 = G2::generator();
+
+    // expect true
+    {
+        // a. single pair
+        {
+            let mut input_bytes: Vec<u8> = vec![];
+
+            let mut bytes_a1 = g1_inf_encoded.clone();
+            let mut bytes_a2 = encode_g2(g2.clone().into_affine()).to_vec();
+            input_bytes.extend(bytes_a1);
+            input_bytes.extend(bytes_a2);
+
+            let input: String = hex::encode(input_bytes.clone());
+
+            let vector = VectorSuccess {
+                input,
+                expected: hex::encode(positive_result_bytes.clone()),
+                name: format!("{}", "g2_pairing_1"),
+            };
+            vectors.push(vector);
+
+            input_bytes.clear();
+            bytes_a1 = encode_g1(g1.clone().into_affine()).to_vec();
+            bytes_a2 = g2_inf_encoded.to_vec().clone();
+            input_bytes.extend(bytes_a1);
+            input_bytes.extend(bytes_a2);
+
+            let input: String = hex::encode(input_bytes.clone());
+
+            let vector = VectorSuccess {
+                input,
+                expected: hex::encode(positive_result_bytes.clone()),
+                name: format!("{}", "g2_pairing_2"),
+            };
+            vectors.push(vector);
+        }
+
+        // b. multiple pair
+        {
+            for i in 0..NUM_TESTS {
+                let mut acc: Fr = Fr::zero();
+                let pair_size: usize = i + 2;
+                let mut input_bytes: Vec<u8> = vec![];
+                // n-1 pairs
+                for _ in 0..pair_size - 1 {
+                    let e1 = Fr::rand(&mut rng);
+                    let e2 = Fr::rand(&mut rng);
+                    let a1 = g1.mul(e1);
+                    let a2 = g2.mul(e2);
+                    let bytes_a1 = encode_g1(a1.into_affine());
+                    let bytes_a2 = encode_g2(a2.into_affine());
+                    input_bytes.extend(bytes_a1);
+                    input_bytes.extend(bytes_a2);
+                    // println!("e1\n{}", e1);
+                    // println!("e2\n{}", e2);
+                    // println!("acc\n{}", acc);
+                    acc += e1 * e2;
+                }
+                // println!("acc\n{}", acc);
+                // last pair
+                let a1 = g1.mul(acc.neg());
+                // println!("nacc\n{}", acc.neg());
+                let a2 = g2;
+                let bytes_a1 = encode_g1(a1.into_affine());
+                let bytes_a2 = encode_g2(a2.into_affine());
+                input_bytes.extend(bytes_a1);
+                input_bytes.extend(bytes_a2);
+
+                let input: String = hex::encode(input_bytes.clone());
+                let result: String = hex::encode(positive_result_bytes.clone());
+
+                let vector = VectorSuccess {
+                    input,
+                    expected: result,
+                    name: format!("{}_{}", "pairing", i + 2),
+                };
+                vectors.push(vector);
+            }
+        }
+    }
+
+    // expect false
+    {
+        for i in 0..NUM_TESTS {
+            let pair_size: usize = i + 1;
+            let mut input_bytes: Vec<u8> = vec![];
+            for _ in 0..pair_size {
+                let e1 = Fr::rand(&mut rng);
+                let e2 = Fr::rand(&mut rng);
+                let a1 = g1.mul(e1);
+                let a2 = g2.mul(e2);
+                let bytes_a1 = encode_g1(a1.into_affine());
+                let bytes_a2 = encode_g2(a2.into_affine());
+                input_bytes.extend(bytes_a1);
+                input_bytes.extend(bytes_a2);
+            }
+
+            let input: String = hex::encode(input_bytes.clone());
+            let result: String = hex::encode(negative_result_bytes.clone());
+
+            let vector = VectorSuccess {
+                input,
+                expected: result,
+                name: format!("{}_{}", "pairing", NUM_TESTS + i + 2),
+            };
+            vectors.push(vector);
+        }
+    }
+
+    write_vectors(vectors, "Pairing");
+}
+fn gen_fail_g1_add_vectors() {
+    let mut rng = test_rng();
+    let input_len = 4 * WORD_SIZE;
+    let pad_zeros: Vec<u8> = vec![0u8; WORD_SIZE - FE_SIZE];
+
+    let mut vectors: Vec<VectorFail> = gen_fail_vectors(input_len);
+
+    // large modulus
+    {
+        let a = G1::rand(&mut rng);
+
+        let mut input_bytes: Vec<u8> = vec![];
+        let a_bytes = encode_g1(a.into_affine());
+        input_bytes.extend(a_bytes);
+        input_bytes.extend(pad_zeros.clone());
+        input_bytes.extend(number_larger_than_modulus());
+        input_bytes.extend(vec![0u8; WORD_SIZE]);
+
+        let input: String = hex::encode(input_bytes.clone());
+        let vector = VectorFail {
+            input,
+            expected_error: String::from("invliad Fq"),
+            name: format!("large_field_element"),
+        };
+        vectors.push(vector);
+    }
+
+    // not on curve
+    {
+        let a = G1::rand(&mut rng);
+        let b = rand_g1_point_not_on_curve();
+
+        let a_bytes = encode_g1(a.into());
+        let e_bytes = encode_g1(b.into());
+
+        let mut input_bytes: Vec<u8> = vec![];
+        input_bytes.extend(a_bytes);
+        input_bytes.extend(e_bytes);
+
+        let input: String = hex::encode(input_bytes.clone());
+        let vector = VectorFail {
+            input,
+            expected_error: String::from("point is not on curve"),
+            name: format!("point_not_on_curve"),
+        };
+        vectors.push(vector);
+    }
+    write_vectors_fail(vectors, "G1Add_Fail");
 }
 
 // #[test]
 // fn generate_test_vectors() {
-//     gen_fail_g1_add_vectors();
-//     gen_fail_g1_mul_vectors();
-//     gen_fail_g1_multiexp_vectors();
-//     gen_fail_g2_add_vectors();
-//     gen_fail_g2_mul_vectors();
-//     gen_fail_g2_multiexp_vectors();
-//     gen_fail_pairing();
+//     gen_g1_add_vectors();
+//     gen_g1_mul_vectors();
+//     gen_g1_multiexp_vectors();
+//     gen_g2_add_vectors();
+//     gen_g2_mul_vectors();
+//     gen_g2_multiexp_vectors();
+//     gen_pairing_vectors();
 // }
+
+#[test]
+fn generate_fail_test_vectors() {
+    gen_fail_g1_add_vectors();
+    // gen_fail_g1_mul_vectors();
+    // gen_fail_g1_multiexp_vectors();
+    // gen_fail_g2_add_vectors();
+    // gen_fail_g2_mul_vectors();
+    // gen_fail_g2_multiexp_vectors();
+    // gen_fail_pairing();
+}
