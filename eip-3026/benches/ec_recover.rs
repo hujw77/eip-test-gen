7759e5852db64c9296dae390fd023b45f0d2f5fa
echo <hujw77@gmail.com>
2023-05-08 15:24:01 +0800

    add ec_recover base beanch

---
+++ eip-3026/benches/ec_recover.rs
@@ -0,0 +1,29 @@
+use criterion::*;
+use parity_crypto::publickey::{recover as ec_recover, sign, Generator, Message, Random};
+
+fn bench_ecrevocer(c: &mut Criterion) {
+	const SAMPLES: usize = 100000;
+
+	let keypairs = (0..SAMPLES).map(|_| Random.generate());
+	let message = Message::zero();
+	let signatures = keypairs
+		.map(|keypair| sign(keypair.secret(), &Message::zero()).unwrap())
+		.collect::<Vec<_>>();
+
+	let mut group = c.benchmark_group("ECRECOVER");
+	group.bench_function(&format!("ECRECOVER for {SAMPLES} samples"), |b| {
+		let mut i = 0;
+		b.iter(|| {
+			i = (i + 1) % SAMPLES;
+			ec_recover(&signatures[i], &message).unwrap()
+		})
+	});
+}
+
+criterion_group! {
+	name = ecrecover;
+	config = Criterion::default();
+	targets = bench_ecrevocer
+}
+
+criterion_main!(ecrecover);
